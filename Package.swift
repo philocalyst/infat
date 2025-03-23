cf5bee388ece766315dd8d03110d84689f50919c
Miles Wirht <regime_skylark.0y@icloud.com>
2025-03-23 09:19:25 -0400

    Added libs
    Plistkit
    swifttoml

--- Package.swift
+++ Package.swift
@@ -8,11 +8,15 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.2.0"),
         .package(url: "https://github.com/apple/swift-log.git", from: "1.5.3"),
+        .package(url: "https://github.com/orchetect/PListKit", from: "2.0.3"),
+        .package(url: "https://github.com/jdfergason/swift-toml", from: "1.0.0"),
     ],
     targets: [
         .executableTarget(
             name: "bart",
             dependencies: [
+                .product(name: "PListKit", package: "PListKit"),
+                .product(name: "Toml", package: "swift-toml"),
                 .product(name: "ArgumentParser", package: "swift-argument-parser"),
                 .product(name: "Logging", package: "swift-log"),
             ]
