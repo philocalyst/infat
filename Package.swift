9010667e39043af56ebc4c85909ba2e507ac70e6
Miles Wirht <regime_skylark.0y@icloud.com>
2025-03-23 16:37:18 -0400

    Defaults to macOS v13
    For maintence purposes. There is no real need for it to be that high,
    it's mainly for access to a few functions which I could recreate in
    time, but I really just want to get this out in the world first.

--- Package.swift
+++ Package.swift
@@ -5,6 +5,10 @@ import PackageDescription
 
 let package = Package(
     name: "bart",
+    platforms: [
+        .macOS(.v13)
+    ],
+
     dependencies: [
         .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.2.0"),
         .package(url: "https://github.com/apple/swift-log.git", from: "1.5.3"),
