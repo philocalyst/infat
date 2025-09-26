01aa69a8499b972375d7a2eb5f71ef8832526cde
Myles Wirth <regime_skylark.0y@icloud.com>
2025-09-26 10:21:15 -0400

    fixed version errors and script errors

--- .config/ci/release/repo-install.sh
+++ .config/ci/release/repo-install.sh
@@ -1,4 +1,4 @@
-nix develop --command gh release download "$TAG_NAME" \\
-  --repo "$GITHUB_REPOSITORY" \\
-  --pattern '*' \\
+nix develop --command gh release download "$TAG_NAME" \
+  --repo "$GITHUB_REPOSITORY" \
+  --pattern '*' \
   --dir dist
