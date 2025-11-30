51caea702a0887c8c7567988efba874e86d7ad7a
Austin Horstman <khaneliman12@gmail.com>
2025-11-30 17:18:56 -0600

    flake.nix: nixpkgs switch to nixpkgs-unstable
    Preferred branch for darwin by darwin maintainers in nixpkgs.
    
    Signed-off-by: Austin Horstman <khaneliman12@gmail.com>

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     fenix.url = "github:nix-community/fenix";
     organist.url = "github:nickel-lang/organist";
   };
