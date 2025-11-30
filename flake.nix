42dc3d53b781d93496fcc2705d4dafbae3868033
Austin Horstman <khaneliman12@gmail.com>
2025-11-30 17:17:42 -0600

    flake.nix: format
    Format with latest nixfmt.
    
    Signed-off-by: Austin Horstman <khaneliman12@gmail.com>

--- flake.nix
+++ flake.nix
@@ -4,10 +4,15 @@
   inputs.organist.url = "github:nickel-lang/organist";
 
   nixConfig = {
-    extra-substituters = ["https://organist.cachix.org" "https://nix-community.cachix.org"];
-    extra-trusted-public-keys = ["organist.cachix.org-1:GB9gOx3rbGl7YEh6DwOscD1+E/Gc5ZCnzqwObNH2Faw=" "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="];
+    extra-substituters = [
+      "https://organist.cachix.org"
+      "https://nix-community.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "organist.cachix.org-1:GB9gOx3rbGl7YEh6DwOscD1+E/Gc5ZCnzqwObNH2Faw="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
   };
 
-  outputs = {organist, ...} @ inputs:
-    organist.flake.outputsFromNickel .config/. inputs {};
+  outputs = { organist, ... }@inputs: organist.flake.outputsFromNickel .config/. inputs { };
 }
